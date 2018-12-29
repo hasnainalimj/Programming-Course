8bb7f7422bd033baa4717bc7b6e0698725e91dfe
hasnainalimj <mjhasnain15@gmail.com>
2018-12-30 03:34:02 +0500

    Class 9

---
+++ Class 9/script.js
@@ -0,0 +1,56 @@
+// function num() {
+//   for (var i = 0; i <= 5; i++) {
+//     console.log(2 + " X " + (i + 1) + "=" + (2 * (i + 1)))
+//   }
+// }
+
+// function num(table, length) {
+//   for (var i = 1; i <= length; i++) {
+//     console.log(table + " X " + i + "=" + (table * i));
+//   }
+// }
+
+// function say() {
+//   return "Hello";
+// }
+
+// function say() {
+//   console.log("Hello");
+// }
+
+// function abc() {
+//   return say()
+// }
+
+// function sum(num1, num2) {
+//   return num1 + num2
+// }
+
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+
+// var result = sum(5, 25);
+
+// var my = function sum(){
+//   return 2+2;
+//   }
+
+
+function  varify(age){
+  if(age <= 10){
+    return "Not Allowed"
+  }
+  else if(age <= 50){
+    return "Allowed"
+  }
+  else if(age > 50){
+    return "Free user"
+  }
+  else{
+    return "Invalid input"
+  }
+}
+
+var age = 10;
+age>=6 ? console.log("That's true") : console.log("That's false");
